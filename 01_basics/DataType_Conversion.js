07afd20331619f0e8e0a9d89854eb40823acab67
coderKundu789 <166862495+coderKundu789@users.noreply.github.com>
2024-08-17 16:07:35 +0000

    Conversion of Data Types

---
+++ 01_basics/DataType_Conversion.js
@@ -0,0 +1,29 @@
+let Somenumber=33
+console.log(Somenumber);
+//Conversion
+//1=>true
+//tathagata like string gives true
+//empty string will give false;
+let isLoggedIn=1
+let booleanLoggedIn=Boolean(isLoggedIn);
+console.log(booleanLoggedIn);
+
+let name="tathagata"
+let booleanName=Boolean(name);
+console.log(booleanName);
+
+let emptyString="";
+let booleanEmptyString=Boolean(emptyString);
+console.log(booleanEmptyString);
+
+let number=333;
+let NumbertoString=String(number);
+console.log(NumbertoString);
+console.log(typeof NumbertoString);
+
+
+
+
+
+
+
